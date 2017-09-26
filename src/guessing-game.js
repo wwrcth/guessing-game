8c65c3a41e0ce73997f22c7814b486ed6b0c1fa7
Nestserava Katsiaryna <wwrcth@gmail.com>
2017-09-26 18:52:22 +0300

    Modified existing file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.low = 0;
+      this.mid = 0;
+      this.high = 0;
+    }
 
     setRange(min, max) {
-
+      this.low = min;
+      this.high = max;
     }
 
     guess() {
-
+      this.mid = Math.ceil((this.low + this.high) / 2);
+      return this.mid;
     }
 
     lower() {
-
+      this.high = this.mid;
     }
 
     greater() {
-
+      this.low = this.mid;
     }
 }
 
